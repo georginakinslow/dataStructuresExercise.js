fb6a28505f9f1e41215b9e1ef912f69ea8c84387
Gina Kinslow <ginakinslow@BCs-MacBook-Pro-6.local>
2020-10-21 09:36:17 -0500

    Created project

---
+++ dataStructuresExercise.js
@@ -0,0 +1,35 @@
+const Jedi = [];
+Jedi[Jedi.length] = "Luke";
+Jedi = ["Luke","Obi-Wan Kenobi"];
+Jedi.unshift("Yoda");
+const Jedi = [
+    ["Luke", "Obi-Wan Kenobi", "Yoda"]
+];
+const Jedi = ["Luke", "Obi-Wan Kenobi", "Yoda"];
+Jedi.splice(3, 2, "Obi-Wan Kenobi");
+console.log(Jedi);
+
+Jedi.slice(2, 2, 2);
+console.log(Jedi(1,));
+
+let sithLords = ["Darth Vader", "Darth Sidious", "Darth Maul"];
+let imperialOfficers = [“Grand Moff Tarkin”, “Orson Krennic”];
+const sithLords, imperialOfficers = concat (starWarsVillians);console.log(starWarsVillians);
+
+"Darth Vader".slice(1,1); "Darth Sidious".slice(2,2);console.log(1,2);
+
+const Droids=[astromech: “R2-D2”), (protocol: “C-3PO”), and (assassin: “IG-88”];
+console.log(Droids [0]);
+
+console.log(Droids[1]);
+
+Droids.splice(3, "IG-11");
+
+console.log["Darth Vader" [6]];
+
+sithLords.slice(-12);
+console.log(sithLords.slice (-12));
+
+const starWarsMovies=[ 1st OBJECT (episodeOne: “The Phantom Menace”, episodeTwo: “Attack of the Clones”, and episodeThree: “Revenge of the Sith”, ). 2nd OBJECT (episodeFour: “A New Hope”, episodeFive: “The Empire Strikes Back”, and episodeSix: “Return of the Jedi”, ). 3rd OBJECT (episodeSeven: “The Force Awakens”, episodeEight: “The Last Jedi”, and  episodeNine: “The Rise of Skywalker”, );
+
+    starWarsMovies.splice(1, 2 "Solo", "Rogue One");
